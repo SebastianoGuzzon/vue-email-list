c639904fdb68473d19d9785d37af44c0bf198282
SebastianoGuzzon <sebyguzz@icloud.com>
2023-04-26 00:17:55 +0200

    Script conclusion

--- js/script.js
+++ js/script.js
@@ -1,12 +1,50 @@
 // - Generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 
 // Attraverso l’apposita API di Boolean
+
 const {createApp} = Vue;
 
 const app = createApp({
     data(){
         return{
             mails: [],
-            numb: 40
+            numb: 10
+// 10 indirizzi da generare
         }
     },
+    methods:{
+        generateMail(){
+            // Generare 10 indirizzi email
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((element)=>{
+                this.mails.push(element.data.response)
+                console.log(this.mails.length);
+                if(this.numb == this.mails.length){
+                    this.$nextTick(()=>{
+                        this.$refs.adress[this.$refs.adress.length-1].scrollIntoView({behavior: "smooth"})
+                        console.log(this.$refs.adress[this.$refs.adress.length-1])
+                    })
+                }
+            });
+        },
+        generateMails(){
+            this.mails=[];
+            for(let i =0; i<this.numb;i++){
+                this.generateMail();
+            }
+        },
+        addMail(){
+            this.numb++;
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((element)=>{
+                this.mails.push(element.data.response)
+                console.log(this.mails.length);
+                if(this.numb == this.mails.length){
+                        this.$nextTick(()=>{
+                        this.$refs.adress[this.$refs.adress.length-1].scrollIntoView({behavior: "smooth"})
+                        console.log(this.$refs.adress[this.$refs.adress.length-1])
+                    })
+                }
+            });
+        }
+    }
+})
+app.mount('#app');
