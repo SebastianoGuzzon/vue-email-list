a09f4c8e5084edf3dce9fb1da7f3226feb1da465
SebastianoGuzzon <sebyguzz@icloud.com>
2023-04-25 23:33:16 +0200

    Modifiche Script

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 // - Generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 // Attraverso l’apposita API di Boolean
+
+const App = new Vue {
+  el: "root",
+  data: {
+    randomMail: []
+  },
+
+  mounted: function () {
+    for (let i = 0; i < 10; i++) {
+      axios
+
+      // Mail di Boolean
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      
+      .then( (result) => {
+
+      }
+      )
+    }
+    
+  }
+
+}
