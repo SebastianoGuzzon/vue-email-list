f1946c8baf624969a7e8399b063627ededc0e049
SebastianoGuzzon <sebyguzz@icloud.com>
2023-04-25 23:53:07 +0200

    First Steps Script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 // - Generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 
 // Attraverso l’apposita API di Boolean
+const {createApp} = Vue;
+
+const app = createApp({
+    data(){
+        return{
+            mails: [],
+            numb: 40
+        }
+    },
